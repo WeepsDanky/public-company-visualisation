0b8a42f3332f9af77bc5e6703a2792e7c4ebfaaa
WeepsDanky <xiaoyi.sun22@imperial.ac.uk>
2023-12-01 01:17:50 +0000

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dqcmsycs5/**',
+      },
+    ],
+  },
+}
+
+
